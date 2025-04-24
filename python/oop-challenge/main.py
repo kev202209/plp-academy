718f38e308cb4f2308a96096ca05f0fa1f9a9dfa
Kelvin Muendo <116151809+kev202209@users.noreply.github.com>
2025-04-24 12:59:49 +0300

    added the oop challenge code

---
+++ python/oop-challenge/main.py
@@ -0,0 +1,17 @@
+from pet import Pet
+
+
+my_pet = Pet("My_pet")
+
+
+my_pet.get_status()
+my_pet.eat()
+my_pet.play()
+my_pet.sleep()
+my_pet.get_status()
+
+my_pet.train("Roll Over")
+my_pet.train("Shake")
+my_pet.show_tricks()
+my_pet.train("Roll Over")
+my_pet.play()
